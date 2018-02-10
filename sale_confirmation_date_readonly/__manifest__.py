572a7ae0462298b2b34aad089b29a8b0da21301d
Alex Lyngsoe <alyngsoe@users.noreply.github.com>
2018-02-10 10:33:37 +0100

    sale_confirmation_date_readonly module
    sale_confirmation_date_readonly module, new

---
+++ sale_confirmation_date_readonly/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Sale Confirmation Date Read only',
+    'version' : '1.0',
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'author': 'Alex Lyngsoe',
+    'website': 'www.odoo.com',
+    'summary': 'Sale Confirmation Date Read only',
+    'description' : '''
+
+Sale Confirmation Date Read only
+''',
+    'depends' : ['sale'],
+    'data' : [
+              ],
+    'installable' : True,
+    'auto_install' : False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
